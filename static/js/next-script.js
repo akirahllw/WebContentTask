31b53d08e4e57a09dfbead5d3e62bf1a241a74be
akirahllw <levonchyckl@gmail.com>
2025-01-16 23:56:54 +0100

    finishing scripts and views.py

--- static/js/next-script.js
+++ static/js/next-script.js
@@ -1,9 +1,52 @@
 function characterChance(){
-       const images = [
+
+       const images_fire = [
            '/static/img/azula.jpg',
            '/static/img/iroh.jpg',
            '/static/img/zuko.jpg',
        ];
+
+       const images_water = [
+           '/static/img/korra.png',
+           '/static/img/katara.png',
+           '/static/img/unalaq.png',
+       ];
+
+       const images_earth = [
+           '/static/img/toff.png',
+           '/static/img/bumi.png',
+           '/static/img/daili.png',
+           '/static/img/kioshi.png',
+       ];
+
+       const images_air = [
+           '/static/img/aang.png',
+           '/static/img/tenzin.png',
+           '/static/img/zahir.png',
+       ];
+
+    let images = []
+
+    const element = document.getElementById('element').innerText.trim().toLowerCase();
+
+    switch (element){
+
+        case 'fire':
+            images = images_fire;
+            break;
+
+        case 'water':
+            images = images_water;
+            break;
+
+        case 'air':
+            images = images_air;
+            break;
+
+        case 'earth':
+            images = images_earth;
+            break;
+    }
 
 const random = Math.floor(Math.random() * images.length);
 const image = images[random];
--- views.py
+++ views.py
@@ -12,7 +12,6 @@ def main(request):
 
 
 def next_page(request):
-  template = loader.get_template('next.html')
   element_input = request.GET.get('element').lower()
   emoji_input = request.GET.get('emojis', '').lower()
 
@@ -23,13 +22,46 @@ def next_page(request):
   return render(request, 'next.html',{'element': element_input})
 
 def random_image_view(request):
-    images = [
+
+    element_input = request.GET.get('element', '').lower()
+
+    images_fire = [
       static('img/azula.jpg'),
       static('img/iroh.jpg'),
       static('img/zuko.jpg'),
     ]
 
+    images_water = [
+        static('img/korra.png'),
+        static('img/katara.png'),
+        static('img/unalaq.png'),
+    ]
+
+    images_earth = [
+        static('img/toff.png'),
+        static('img/bumi.png'),
+        static('img/daili.png'),
+        static('img/kioshi.png'),
+    ]
+
+    images_air = [
+        static('img/aang.png'),
+        static('img/tenzin.png'),
+        static('img/zahir.png'),
+    ]
+
+    element_images = {
+        'fire': images_fire,
+        'water': images_water,
+        'earth': images_earth,
+        'air': images_air,
+    }
+
+    images = element_images.get(element_input, images_air)
 
     random_image = random.choice(images)
-    random_image_input = request.GET.get('image_input',)
-    return render(request, 'next.html', {'random_image': random_image})
+
+    return render(request, 'next.html', {
+        'random_image': random_image,
+        'element': element_input,
+    })
